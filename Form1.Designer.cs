7cb868d61ee82b4f84b273518532c354dc534954
zloihach <72695485+zloihach@users.noreply.github.com>
2022-11-30 19:28:22 +0300

    done lab5

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Fast_GPU_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openGLControl1 = new SharpGL.OpenGLControl();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxInputX = new System.Windows.Forms.TextBox();
+            this.textBoxInputY = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxInoutZ = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLControl1
+            // 
+            this.openGLControl1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.openGLControl1.DrawFPS = true;
+            this.openGLControl1.FrameRate = 60;
+            this.openGLControl1.Location = new System.Drawing.Point(-6, -3);
+            this.openGLControl1.Name = "openGLControl1";
+            this.openGLControl1.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL4_4;
+            this.openGLControl1.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl1.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl1.Size = new System.Drawing.Size(915, 469);
+            this.openGLControl1.TabIndex = 1;
+            this.openGLControl1.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl1_OpenGLDraw);
+            this.openGLControl1.Load += new System.EventHandler(this.openGLControl1_Load);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(6, 475);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "X :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxInputX
+            // 
+            this.textBoxInputX.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxInputX.Location = new System.Drawing.Point(44, 472);
+            this.textBoxInputX.Name = "textBoxInputX";
+            this.textBoxInputX.Size = new System.Drawing.Size(239, 25);
+            this.textBoxInputX.TabIndex = 4;
+            this.textBoxInputX.Text = "1";
+            this.textBoxInputX.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // textBoxInputY
+            // 
+            this.textBoxInputY.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxInputY.Location = new System.Drawing.Point(350, 472);
+            this.textBoxInputY.Name = "textBoxInputY";
+            this.textBoxInputY.Size = new System.Drawing.Size(239, 25);
+            this.textBoxInputY.TabIndex = 6;
+            this.textBoxInputY.Text = "1";
+            this.textBoxInputY.TextChanged += new System.EventHandler(this.textBoxInputY_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(312, 475);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Y :";
+            // 
+            // textBoxInoutZ
+            // 
+            this.textBoxInoutZ.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxInoutZ.Location = new System.Drawing.Point(658, 472);
+            this.textBoxInoutZ.Name = "textBoxInoutZ";
+            this.textBoxInoutZ.Size = new System.Drawing.Size(239, 25);
+            this.textBoxInoutZ.TabIndex = 8;
+            this.textBoxInoutZ.Text = "1";
+            this.textBoxInoutZ.TextChanged += new System.EventHandler(this.textBoxInoutZ_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(620, 475);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Z :";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(900, 500);
+            this.Controls.Add(this.textBoxInoutZ);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxInputY);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxInputX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.openGLControl1);
+            this.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private SharpGL.OpenGLControl openGLControl1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxInputX;
+        private System.Windows.Forms.TextBox textBoxInputY;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxInoutZ;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
